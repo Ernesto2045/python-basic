69a87b4a40d5c868c248dc0c40c1346cf844625f
Liuver Duran <liuver@gmail.com>
2024-01-16 14:54:51 -0600

    update: new folder exercises

---
+++ exercises/conditionals/pizza.py
@@ -0,0 +1,17 @@
+'''
+Exercise 1:
+    The Bella Napoli pizzeria offers vegetarian and non-vegetarian pizzas to its customers. 
+    The ingredients for each type of pizza are listed below.
+
+    Vegetarian ingredients: Pepper and tofu.
+    Non-vegetarian ingredients: Pepperoni, Ham, and Salmon.
+
+    Write a program that asks the user whether they want a vegetarian pizza or not, and based on their response, 
+    show them a menu with the available ingredients for them to choose from. 
+    Only one ingredient can be chosen in addition to the mozzarella and tomato that are on all pizzas. 
+    At the end, the program should display whether the chosen pizza is vegetarian or not and all the ingredients it contains.
+        
+'''
+
+
+
---
+++ exercises/loops/_guide.py
@@ -0,0 +1,53 @@
+'''
+Exercise 1
+    Write a program that asks the user for a word and displays it on the screen 10 times.
+
+Exercise 2
+    Write a program that asks the user for their age and displays on the screen every year they have reached (from 1 to their age).
+
+Exercise 3
+    Write a program that asks the user for a positive integer and displays on the screen all the odd numbers from 1 to that number, separated by commas.
+
+Exercise 4
+    Write a program that asks the user for a positive integer and displays on the screen a countdown from that number to zero, separated by commas.
+
+Exercise 5
+    Write a program that asks the user for an amount to invest, the annual interest rate, and the number of years, and shows on the screen the capital obtained from the investment each year during the investment period.
+
+Exercise 6
+    Write a program that asks the user for an integer and displays on the screen a right triangle like the one below, with the height being the number entered.
+    *
+    **
+    ***
+    ****
+    *****
+    ******
+
+Exercise 7
+    Write a program that displays on the screen the multiplication table from 1 to 10.
+
+Exercise 8
+    Write a program that asks the user for an integer and displays on the screen a right triangle like the one below.
+
+    1
+    3 1
+    5 3 1
+    7 5 3 1
+    9 7 5 3 1
+
+Exercise 9
+    Write a program that stores the string 'password' in a variable, asks the user for the password until they enter the correct password.
+
+Exercise 10
+    Write a program that asks the user for an integer and displays on the screen whether it is a prime number or not.
+
+Exercise 11
+    Write a program that asks the user for a word and then displays on the screen one by one the letters of the word entered starting with the last.
+
+Exercise 12
+    Write a program that asks the user for a phrase and a letter, and displays on the screen the number of times the letter appears in the phrase.
+
+Exercise 13
+    Write a program that echoes everything the user enters until the user writes “exit” which will end it.
+
+'''
